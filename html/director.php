b7f5e7ae6b251de409cd075cea8e21f58882151f
JeNaye <jenaye.johnson@gmail.com>
2009-05-24 22:01:24 -0400

    Fixed director page.

--- html/director.php
+++ html/director.php
@@ -2,8 +2,14 @@
 $require_login = false;
 require_once 'include/set_env.php';
 $t->assign('title', 'Duke University Marching & Pep Band');
-$t->assign('include_file', 'content/director.tpl');
-$t->assign('front', $db->getAll("select * from dumb_content where loc='director' order by date desc"));
+$t->assign('include_file', 'content/generic.tpl');
+$t->assign('sidebar', 'sidebar/leadership.tpl');
+
+$t->assign('front', $db->getAll("select * from dumb_content where loc='director'"));
+
 $t->assign('loc', 'leadership');
+
+$t->assign('breadcrumb', "<a href='index.php'>Home</a> > Leadership");
+
 $t->display('main.tpl');
 ?>
